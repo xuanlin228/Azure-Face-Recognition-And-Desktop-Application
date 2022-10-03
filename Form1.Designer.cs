e20630c016f9c9ff4e0e5def1f2999881e0dab00
xuanlin228 <yunxuanlin228@gmail.com>
2022-10-03 10:37:25 +0800

    windows form to build the GUI

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace facewin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(12, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "face info";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(189, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(337, 25);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(659, 40);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(458, 198);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(12, 98);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(623, 486);
+            this.panel1.TabIndex = 5;
+            this.panel1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.AutoUpgradeEnabled = false;
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label1.Location = new System.Drawing.Point(655, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Dataset";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(659, 338);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(485, 470);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 853);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Web.Script.Serialization;
+
+namespace facewin
+{
+    
+    public partial class Form1 : Form
+    {
+        string faceApiKey;
+        string faceApiUrl = "faceApiurl";
+        string imagePath;
+        FaceServiceClient faceServiceClient;
+        Face[] objFace;
+        string _groupId;
+        string list_result;
+        string faceid;
+        List<Person> Persons = new List<Person>();
+        public Form1(/*string _faceApiKey*/)
+        {
+
+            //faceApiKey = _faceApiKey;
+            InitializeComponent();
+            faceApiKey = "faceapiKey";
+            button2.Visible = false;
+            _groupId = "face03";
+            
+        }
+        private async Task<string> GetFaceId(string _imagePath, string _faceApiUrl, string _faceApiKey)
+        {
+            string str = "";
+            faceServiceClient = new FaceServiceClient(_faceApiKey, _faceApiUrl);
+            
+            FaceAttributeType[] fa = new FaceAttributeType[]
+            {
+                FaceAttributeType.Age,
+                FaceAttributeType.Gender,
+                FaceAttributeType.Emotion,
+            };
+            
+            objFace = await faceServiceClient.DetectAsync(File.OpenRead(imagePath), true, false, fa);
+            if (objFace.Length > 0)
+                for (int i = 0; i < objFace.Length; i++)
+                    str += "\"" + objFace[i].FaceId.ToString()+"\"" + ",";
+            else
+                str = "無法辨識，重新選擇";
+            return str;
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+            string filename = openFileDialog1.FileName;
+            textBox2.Text = "";
+            richTextBox1.Text = "";
+            if (dr == System.Windows.Forms.DialogResult.OK && !string.IsNullOrEmpty(filename))
+            {
+                imagePath = openFileDialog1.FileName;
+
+                panel1.BackgroundImage = new Bitmap(imagePath);
+                panel1.BackgroundImageLayout = ImageLayout.Zoom;
+                panel1.Refresh();
+                textBox1.Text = imagePath;
+                faceid = await GetFaceId(imagePath, faceApiUrl, faceApiKey);
+                button2.Visible = true;
+            }
+            
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (objFace.Length > 0)
+            {
+                
+                for (int i = 0; i < objFace.Length; i++)
+                {
+                    FaceRectangle fRectangle = objFace[i].FaceRectangle;
+                    double BImageWidth = panel1.BackgroundImage.Width;
+                    double BImageHeight = panel1.BackgroundImage.Height;
+
+                    //care of coordinate change
+                    double xx = 0, yy = 0;
+                    double scale = panel1.Width / BImageWidth;
+                    if (scale >= panel1.Height / BImageHeight)
+                    {
+                        scale = panel1.Height / BImageHeight;
+                        xx = (panel1.Width - scale * BImageWidth) / 2.0;
+                    }
+                    else
+                    {
+                        yy = (panel1.Height - scale * BImageHeight) / 2.0;
+                    }
+                    Graphics g = panel1.CreateGraphics();
+                    Pen[] p = new Pen[2];
+                    p[0] = new Pen(Color.Yellow, 3);
+                    p[1] = new Pen(Color.Blue, 3);
+                    int left, top, width, height;
+
+                    left = (int)(panel1.Width * fRectangle.Left);
+                    top = (int)(panel1.Height * fRectangle.Top);
+                    width = (int)(panel1.Width * fRectangle.Height);
+                    height = (int)(panel1.Height * fRectangle.Width);
+                    g.DrawRectangle(p[i % 2], left, top, width, height);
+
+                    
+
+
+                    string strinfo = $"性別:{objFace[i].FaceAttributes.Gender }\r\n" +
+                    $"年齡: {objFace[i].FaceAttributes.Age}\r\n============\r\n";
+                    identify(strinfo);
+                    //textBox2.Text += strinfo;
+                    var PersonGroup =await list();
+                    for(int k=0;k<PersonGroup.Count;k++)
+                    {
+                        richTextBox1.SelectionColor = Color.BlueViolet;
+                        richTextBox1.Text +="Url: "+ PersonGroup[k].Userdata+"\r\n";
+                        richTextBox1.SelectionColor = Color.RosyBrown;
+                        richTextBox1.Text += "Name: " + PersonGroup[k].Name + "\r\n";
+                        richTextBox1.SelectionColor = Color.Black;
+                        richTextBox1.Text += "\r\n===========\r\n";
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Choose a photo with face\r\n");
+            }
+        }
+        private async void identify(string info)
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "3e652220978f4479b3258dd90c71d8ce");
+
+            var uri = "https://eastasia.api.cognitive.microsoft.com/face/v1.0/identify?" + queryString;
+
+            HttpResponseMessage response;
+            //string id= await GetFaceId(imagePath, faceApiUrl, faceApiKey); ;
+            string body = "{\"PersonGroupId\": \"face03\",\"faceIds\": [" + faceid + "],\"maxNumOfCandidatesReturned\": 1,\"confidenceThreshold\": 0.5}";
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes(body);
+            string result;
+            JArray jArray;
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
+                result = await response.Content.ReadAsStringAsync();
+                jArray = JArray.Parse(result);
+            }
+            string faceId;
+            JToken candidates;
+            foreach (JObject json in jArray)
+            {
+                int i = 0;
+                faceId = json.GetValue("faceId").ToString();
+                candidates = json.GetValue("candidates");
+                string name = "";
+                foreach (JObject j in candidates)
+                {
+                    string personId;
+                    string confidence;
+                    i++;
+                    personId = j.GetValue("personId").ToString();
+                    confidence = j.GetValue("confidence").ToString();
+                    //textBox2.Text += "personid: " + confidence + "\r\n";
+                    textBox2.Text += "confidence: " + confidence + "\r\n";
+
+                    var PersonGroup = await list();
+                    Console.WriteLine(PersonGroup.Count);
+                    for (int k = 0; k < PersonGroup.Count; k++)
+                    {
+                        if (personId == PersonGroup[k].Personid)
+                        {
+                            name = Persons[k].Name;
+                        }
+                    }
+                    //textBox2.Text += "Candidates: "+ candidates + "\r\n";
+                }
+                if (i == 0)
+                {
+                    textBox2.Text += "confidence:  Sorry~~ Not match person\r\n";
+                    textBox2.Text += "faceId: " + faceId + "\r\n";
+                    textBox2.Text += "\r\n======\r\n";
+                }
+                else
+                {
+                    textBox2.Text += "faceId: " + faceId + "\r\n";
+                    textBox2.Text += "name:" + name + "\r\n";
+                    
+                }
+                //textBox2.Text += "Candidates: "+ candidates + "\r\n";
+
+            }
+            textBox2.Text += info;
+
+        }
+        private async Task<List<Person>> list()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "3e652220978f4479b3258dd90c71d8ce");
+
+            // Request parameters
+            queryString["start"] = "";
+            queryString["top"] = "1000";
+            var uri = "https://eastasia.api.cognitive.microsoft.com/face/v1.0/persongroups/face03/persons?" + queryString;
+            string temp;
+            string result;
+            //JavaScriptSerializer js = new JavaScriptSerializer();
+            var response = await client.GetAsync(uri);
+            temp = await response.Content.ReadAsStringAsync();
+            
+            JArray jArray = JArray.Parse(temp);
+            string personId;
+            string persistedFaceIds;
+            string name="";
+            string userData;
+            
+            foreach (JObject json in jArray)
+            {
+                personId = json.GetValue("personId").ToString();
+                persistedFaceIds = json.GetValue("persistedFaceIds").ToString();
+                name = json.GetValue("name").ToString();
+                userData = json.GetValue("userData").ToString();
+                Person p1 = new Person(personId,name,userData);
+                Persons.Add(p1);
+
+            }
+            return Persons;
+            //Console.WriteLine(Persons.Count);
+            
+
+
+        }
+    }
+    public class Person
+    {
+        private string _name; //姓名
+        private string _personid;
+        private string _userdata;
+                                  
+        public Person(string personid, string Name,string Userdata)
+        {
+            this._name = Name;
+            this._personid = personid;
+            this._userdata = Userdata;
+        }
+        //姓名
+        public string Name
+        {
+            get { return _name; }
+        }
+        //年龄
+        public string Personid
+        {
+            get { return _personid; }
+        }
+        public string Userdata
+        {
+            get { return _userdata; }
+        }
+    }
+}
+
